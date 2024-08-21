80b1db4ae49c27528cede731f75c5de72ceb5fff
sergeniys <sergeykartsev2016@gmail.com>
2024-08-21 20:09:45 +0300

    ыусщтв commit

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template, request, jsonify
-import random
+
 import consolbomb as bomber
 
 
 app = Flask(__name__)
-historsis = []
 f = open(r'russian.txt').readlines()
-phrase = ''
+params = {'historsis': [], 'phrase': '', 'nick': '', }
 
+@app.route('/save_nick', methods=['POST'])
+def save_nick():
+    global params
+    data = request.get_json()
+    params['nick'] = data.get('nick')
+
+    if params['nick'] == '':
+        params['nick'] = "unnamed"
+    return ('', 204)
+
+@app.route('/leaders')
+def leaders():
+    leaders_data = []
+    with open('leaders.txt', 'r') as leaders_file:
+        for line in leaders_file:
+            nick, score = line.strip().split(',')
+            leaders_data.append({'nick': nick, 'score': score})
+    return render_template('leaders.html', leaders=leaders_data)
 @app.route('/')
 def indexhome():
     return render_template('indexhome.html')
@@ -18,8 +35,8 @@ def index():
 
 def create_word():
     word = bomber.create()
-    global phrase
-    phrase = word
+    global params
+    params['phrase'] = word
     return {'word': word}
 
 @app.route('/create_word', methods=['GET'])
@@ -30,26 +47,27 @@ def generate_word():
 
 @app.route('/check_numbers', methods=['POST'])
 def check_numbers():
+    global params
     data = request.get_json()
     text = str(data.get('text', '')).lower()[:-1]
-    hasNumbers = False
-    print('до', phrase, text)
-    #print(str(text.lower())[:-1] == "число")
+    hasnumbers = False
+    #print('до', phrase, text, nick)
+    # #print(str(text.lower())[:-1] == "число")
+    #
+    # if phrase in text:
+    #     print('1')
+    # if f'{text}\n' in f:
+    #     print('2')
+    # if text not in historsis:
+    #     print('3')
 
-    if phrase in text:
-        print('1')
-    if f'{text}\n' in f:
-        print('2')
-    if text not in historsis:
-        print('3')
+    if params['phrase'] in text and f'{text}\n' in f and text not in params['historsis']:
 
-    if phrase in text and f'{text}\n' in f and text not in historsis:
-        print('работает')
-        historsis.append(text)
-        hasNumbers = True
+        params['historsis'].append(text)
+        hasnumbers = True
 
 
-    return jsonify({'hasNumbers': hasNumbers})
+    return jsonify({'hasNumbers': hasnumbers})
 
 if __name__ == '__main__':
     app.run(debug=True)
