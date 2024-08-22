e32cdbe52c6224269d8ddd1f1e01377b9965b24d
sergeniys <sergeykartsev2016@gmail.com>
2024-08-22 22:41:52 +0300

    end  commit pobeda!

--- consolbomb.py
+++ consolbomb.py
@@ -2,12 +2,12 @@
 import random
 from threading import Timer
 f = open(r'russian.txt').readlines()
-letters = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п",
-           "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
-zapret = ['ь', 'ъ', 'ы', 'ф', 'э', 'й']# проще удалить из letters, но это для усложения в дальнейшем
+letters = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
+           'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+zapret = ['ь', 'ъ', 'ы', 'ф', 'э', 'й', 'ё', 'щ', 'ш', 'ю']# проще удалить из letters, но это для усложения в дальнейшем
 
-sogls = ['б', "в", "г", "д", "ж", "з" "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ч", "щ", "ш", "й", "ц"]
-vse_gls = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]
+sogls = ["б", "в", "г", "д", "Ж","з","к", "л", "м", "н", "п", "р", "с","т", "ф", "х", "ц", "ч", "ш", "щ"]
+vse_gls = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
 
 historsis = []  # list of words
 
@@ -18,11 +18,13 @@ historsis = []  # list of words
 
 
 def proverka(choser):
-    if choser[0] in zapret or choser[1] in zapret:
+    if choser[0] in zapret   or choser[1] in zapret:
         return False
     if choser[0] in sogls and choser[1] in sogls:
         return False
-    if choser[1] in vse_gls and choser[0] in vse_gls:
+    if choser[0] in vse_gls and choser[1] in vse_gls:
+        return False
+    if choser == 'чя' or choser == 'чю':
         return False
     return True
 
@@ -31,6 +33,7 @@ def create():
     while True:
         choser = ''.join(random.choices(letters, k=2))
         if proverka(choser):
+            print(choser)
             return choser
 
 
